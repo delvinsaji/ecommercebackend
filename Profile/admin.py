1702e2d8872be5842c7a53a110eb4a1b5c78241f
delvinsaji <91379593+delvinsaji@users.noreply.github.com>
2022-07-10 14:37:30 +0530

    admin

--- Profile/admin.py
+++ Profile/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+
 # Register your models here.
+
+from .models import Profile,Address,Cart,Review,Visited,Orders
+
+admin.site.register([Profile,Address,Cart,Review,Visited,Orders])
